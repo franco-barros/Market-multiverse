db8738906b4012008abec8e180d1159c01f00010
FbarcalaR <50779513+FbarcalaR@users.noreply.github.com>
2024-11-06 15:03:33 +0100

    Update index.jsx

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -18,6 +18,7 @@ const Navbar = () => {
             <NavLink
               onClick={() => context.setSearchByCategory()}
               to="/"
+              // buenisimo esto. no le la sabia tampoco 
               className={({ isActive }) => (isActive ? activeStyle : undefined)}
             >
               All
